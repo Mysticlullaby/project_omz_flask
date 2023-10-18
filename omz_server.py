21219ff02e50bde0da94317aafa581fe07bc839c
Mystic <mystic091717@gmail.com>
2023-10-18 11:09:00 +0900

    20231018 commit

--- omz_server.py
+++ omz_server.py
@@ -21,9 +21,15 @@ def index():
 #    return 'testData'
     query = 'select * from movie'
     df = pd.read_sql_query(query,conn)
-    df.columns = ['movieId', 'title', 'movieDescription', 'image', 'poster', 'trailer', 'castings']
+    df.columns = ['movieId', 'title', 'movieDescription', 'image', 'poster', 'trailer', 'castings', 'provider', 'kinoRating', 'rottenRating', 'imdbRating', 'staff']
     json_data = df.to_json(orient='records', force_ascii=False)
     return jsonify(json_data)
+
+@app.route('/curateByRating')
+def curateByRating():
+    query = 'select * from movie'
+    movies = pd.read_sql_query(query, conn)
+    return 'processing data...'
 
 if __name__ == '__main__':  
    app.run('127.0.0.1',port=5000,debug=True)
